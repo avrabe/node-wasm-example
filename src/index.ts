bdefebf1a35b8ac81ca67358bb88917edd49d760
Ralf Anton Beier <ralf_beier@me.com>
2024-02-06 05:50:36 +0100

    start migrate to inversify
    Signed-off-by: Ralf Anton Beier <ralf_beier@me.com>

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,58 @@
 import { hello, $init } from './handler/hello.js';
-await $init;
-function local_hello(name: string) {
-    let result = hello.Hello.createWorld();
-    for (let i = 0; i < 1000000; i++) {
-        console.log(`Hello  ${result.calls()}!`);
-    }
+import { injectable, inject } from 'inversify';
+
+
+
+interface Logger {
+  log(msg: string): void;
+}
+
+@injectable()
+class ConsoleLogger implements Logger {
+  public log(msg: string) {
+    console.log(msg); 
   }
+}
+
+@injectable()
+class ApiClient {
+    private world;
+  constructor(@inject(ConsoleLogger) private logger: Logger) {
+    this.init();
+    this.world = hello.Hello.createWorld();
+  }
+  async init() {
+    await $init;
+    
+    // do something else
+  }
+
+  public makeApiCall() {
+    this.logger.log("Making API call...");
+  }
+}
+
+
+
+// Set up container
+import { Container } from 'inversify';
+
+const container = new Container();
+container.bind<Logger>('Logger').to(ConsoleLogger);
+container.bind<ApiClient>(ApiClient).toSelf();
+
+// Usage
+const apiClient = container.get<ApiClient>(ApiClient);
+apiClient.makeApiCall();
+
+
+// function local_hello(name: string) {
+//     let result = hello.Hello.createWorld();
+//     for (let i = 0; i < 1000000; i++) {
+//         console.log(`Hello  ${result.calls()}!`);
+//     }
+//   }
   
   
-  local_hello('World');
+//   local_hello('World');
   
