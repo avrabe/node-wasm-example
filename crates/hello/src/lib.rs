bc7d42432aa7f04d28f42dd606c46b39bcbdc121
Ralf Anton Beier <ralf_beier@me.com>
2024-03-19 17:14:09 +0100

    Upgrade to wit-bindgen 0.22
    Reintroducing the generate macro
    
    Signed-off-by: Ralf Anton Beier <ralf_beier@me.com>

--- crates/hello/src/lib.rs
+++ crates/hello/src/lib.rs
@@ -1,16 +1,20 @@
 mod bindings;
 
-use bindings::exports::component::hello::hello::GuestHello;
-
 use std::cell::RefCell;
 
-use cargo_component_bindings::rt::Resource;
+use bindings::exports::component::{
+    self,
+    hello::hello::{Guest, GuestHello, Hello},
+};
 
-pub struct Hello {
+impl Guest for Hello {
+    type Hello = HelloComponent;
+}
+pub struct HelloComponent {
     calls: RefCell<u32>,
 }
 
-impl GuestHello for Hello {
+impl GuestHello for HelloComponent {
     fn calls(&self) -> String {
         self.calls.replace_with(|&mut old| old + 1);
         format!("hello {} times", self.calls.borrow())
@@ -20,10 +24,12 @@ impl GuestHello for Hello {
         "hello".to_string()
     }
 
-    fn create_world() -> Resource<Hello> {
-        let hello = Hello {
+    fn create_world() -> component::hello::hello::Hello {
+        let hello = HelloComponent {
             calls: RefCell::new(0),
         };
-        Resource::<Hello>::new(hello)
+        component::hello::hello::Hello::new(hello)
     }
 }
+
+bindings::export!(Hello with_types_in bindings);
